2c790f1248c64b572166772f2f3ad8694c6f71eb
IgnacioDe <ignacio.id81@gmail.com>
2024-03-05 23:21:07 -0300

    cambios de pantalla

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,46 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Sprite } from 'pixi.js'
 
 const app = new Application<HTMLCanvasElement>({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1280,
+	height: 720
+});
+
+window.addEventListener("resize", ()=>{
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX, scaleY);
+
+	const gameWindth = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
+
+	const margenHorizontal = Math.floor((window.innerWidth - gameWindth) / 2);
+	const margenVerticval = Math.floor((window.innerHeight - gameHeight) / 2);
+
+	app.view.style.width = gameWindth + "px";
+	app.view.style.height = gameHeight + "px";
+
+	app.view.style.marginLeft = margenHorizontal + "px";
+	app.view.style.marginRight = margenHorizontal + "px";
+
+	app.view.style.marginTop = margenVerticval + "px";
+	app.view.style.marginBottom = margenVerticval + "px";
+ 	
+});
+window.dispatchEvent(new Event("resize"));
+
+Loader.shared.add({url: "clampy.png", name: "Image"});
+
+Loader.shared.onComplete.add(()=> {
+	const image: Sprite = Sprite.from("Image");
+
+	image.x = 0;
+	image.y = 0;
+
+	app.stage.addChild(image);
 });
 
 const clampy: Sprite = Sprite.from("clampy.png");
